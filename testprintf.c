a1fbf94b79ed512a21ee6e8d50c154285bbb562f
Lauranne BINTEIN <labintei@e1r12p18.42.fr>
2021-02-18 15:20:57 +0100

    testprintf

--- testprintf.c
+++ testprintf.c
@@ -6,7 +6,7 @@
 /*   By: labintei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 10:37:27 by labintei          #+#    #+#             */
-/*   Updated: 2021/02/18 10:51:46 by labintei         ###   ########.fr       */
+/*   Updated: 2021/02/18 15:20:32 by labintei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int			main(void)
 {
-	printf("%.0d\n", 1);
-	printf("%0*.*dI",5, 5, 256);
+	printf("TEST LARGEUR\n");
+	printf("Pour d (meme comportement u, p, X, x, %%)\n");
+	printf("largeur sans indicateur   I%5dI",5);
+	printf("\nlargeur avec indicateur 0 I%05dI",5);
+	printf("\nlargeur avec indicateur - I%-5dI", 5);
+	printf("\nPour s");
+	printf("\nlargeur sans indicateur   I%5sI", "AB");
+	printf("\nlargeur avec indicateur 0 I%05sI non define par default", "AB");
+	printf("\nlargeur avec indicateur - I%-5sI", "AB");
+	printf("\nTEST PRECISION");
+	printf("\nPour d (meme comportement u, p, X, x, %%)");
+	printf("\nprecision 5 sur nb = 5 : %.5d", 5);
+	printf("\ncomportement sans precision 0 : %d", 0);
+	printf("\ncomportement speciale precision 0 sur 0 : %.0d(vide)", 0);
+	printf("\ncomportement normale precision 0 sur 5 : %.0d", 5);
+	printf("\nPour s");
+	printf("\nprecision 5 sur ILauranne va a la plageI : %.5s", "Lauranne va a la plage");
+	printf("\nprecision 5 sur ILauI : I%.5sI", "Lau");
 	return(1);
 }
